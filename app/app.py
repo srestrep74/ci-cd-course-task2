6a6704f83832d6c967942b64a2662b618b86e2a0
sebastian <srestrep74@eafit.edu.co>
2025-09-13 08:53:42 -0500

    fix http methods unsecure

--- app/app.py
+++ app/app.py
@@ -9,36 +9,43 @@ from .calculadora import sumar, restar, multiplicar, dividir
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
     """
     Renders the main page of the calculator application.
+    """
+    return render_template("index.html", resultado=None)
 
-    Handles GET requests by rendering the index.html template.
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    """
+    Performs a calculation based on user input from the calculator form.
+
     Handles POST requests by performing a calculation based on user input
     and rendering the index.html template with the result.
     """
     resultado = None
-    if request.method == "POST":
-        try:
-            num1 = float(request.form["num1"])
-            num2 = float(request.form["num2"])
-            operacion = request.form["operacion"]
 
-            if operacion == "sumar":
-                resultado = sumar(num1, num2)
-            elif operacion == "restar":
-                resultado = restar(num1, num2)
-            elif operacion == "multiplicar":
-                resultado = multiplicar(num1, num2)
-            elif operacion == "dividir":
-                resultado = dividir(num1, num2)
-            else:
-                resultado = "Operación no válida"
-        except ValueError:
-            resultado = "Error: Introduce números válidos"
-        except ZeroDivisionError:
-            resultado = "Error: No se puede dividir por cero"
+    try:
+        num1 = float(request.form["num1"])
+        num2 = float(request.form["num2"])
+        operacion = request.form["operacion"]
+
+        if operacion == "sumar":
+            resultado = sumar(num1, num2)
+        elif operacion == "restar":
+            resultado = restar(num1, num2)
+        elif operacion == "multiplicar":
+            resultado = multiplicar(num1, num2)
+        elif operacion == "dividir":
+            resultado = dividir(num1, num2)
+        else:
+            resultado = "Operación no válida"
+    except ValueError:
+        resultado = "Error: Introduce números válidos"
+    except ZeroDivisionError:
+        resultado = "Error: No se puede dividir por cero"
 
     return render_template("index.html", resultado=resultado)
 
